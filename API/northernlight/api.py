791ea9b37fc4cea36e82e6ff98dc256601adcf43
Atick Faisal <atickfaisal@gmail.com>
2021-03-26 08:56:14 +0600

    api done

---
+++ API/northernlight/api.py
@@ -0,0 +1,35 @@
+# from .models import UserData
+# from rest_framework import viewsets, permissions
+# from northernlight.seralizers import UserDataSerializer
+#
+#
+# class UserDataViewSet(viewsets.ModelViewSet):
+#     queryset = UserData.objects.all()
+#     serializer_class = UserDataSerializer
+#     permission_classes = [permissions.AllowAny]
+
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.parsers import JSONParser
+from northernlight.models import UserData
+from northernlight.seralizers import UserDataSerializer
+
+
+@csrf_exempt
+def user_data(request):
+    if request.method == 'GET':
+        data = UserData.objects.all()
+        serializer = UserDataSerializer(data, many=True)
+        return JsonResponse(serializer.data, safe=False)
+
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+
+        # --------------- DO THINGS --------------- #
+
+
+        serializer = UserDataSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
---
+++ API/northernlight/urls.py
@@ -0,0 +1,17 @@
+# from rest_framework import routers
+# from .api import UserDataViewSet
+
+# router = routers.DefaultRouter()
+# router.register('api/northernlight', UserDataViewSet, 'UserData')
+#
+# urlpatterns = router.urls
+
+from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
+from northernlight import api
+
+urlpatterns = [
+    path('api/northernlight/', api.user_data)
+]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
